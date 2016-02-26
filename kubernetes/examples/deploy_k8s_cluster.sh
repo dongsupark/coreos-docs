58b5b58969bee787dbb51bfe7a354c0eab3d655c
Dongsu Park <dongsu@endocode.com>
2016-02-26 16:27:00 +0100

    update to the newest version from endocode/kayrus/scripts
    version bump to the newest one from github.com/endocode/kayrus/scripts.

--- kubernetes/examples/deploy_k8s_cluster.sh
+++ kubernetes/examples/deploy_k8s_cluster.sh
@@ -1,12 +1,22 @@
 #!/bin/bash -e
 
 usage() {
-  echo "Usage: $0 %k8s_cluster_size% [%pub_key_path%]"
+  echo "Usage: $0 %cluster_size% [%pub_key_path%]"
 }
 
 print_green() {
   echo -e "\e[92m$1\e[0m"
 }
+
+OS_NAME="coreos"
+
+export LIBVIRT_DEFAULT_URI=qemu:///system
+virsh nodeinfo > /dev/null 2>&1 || (echo "Failed to connect to the libvirt socket"; exit 1)
+virsh list --all --name | grep -q "^${OS_NAME}1$" && (echo "'${OS_NAME}1' VM already exists"; exit 1)
+
+USER_ID=${SUDO_UID:-$(id -u)}
+USER=$(getent passwd "${USER_ID}" | cut -d: -f1)
+HOME=$(getent passwd "${USER_ID}" | cut -d: -f6)
 
 if [ "$1" == "" ]; then
   echo "Cluster size is empty"
@@ -29,126 +39,138 @@ fi
 if [[ -z $2 || ! -f $2 ]]; then
   echo "SSH public key path is not specified"
   if [ -n $HOME ]; then
-        PUB_KEY_PATH="$HOME/.ssh/id_rsa.pub"
+    PUB_KEY_PATH="$HOME/.ssh/id_rsa.pub"
   else
-        echo "Can not determine home directory for SSH pub key path"
-        exit 1
+    echo "Can not determine home directory for SSH pub key path"
+    exit 1
   fi
 
   print_green "Will use default path to SSH public key: $PUB_KEY_PATH"
   if [ ! -f $PUB_KEY_PATH ]; then
-        echo "Path $PUB_KEY_PATH doesn't exist"
-        exit 1
+    echo "Path $PUB_KEY_PATH doesn't exist"
+    PRIV_KEY_PATH=$(echo ${PUB_KEY_PATH} | sed 's#.pub##')
+    if [ -f $PRIV_KEY_PATH ]; then
+      echo "Found private key, generating public key..."
+      sudo -u $USER ssh-keygen -y -f $PRIV_KEY_PATH | sudo -u $USER tee ${PUB_KEY_PATH} > /dev/null
+    else
+      echo "Generating private and public keys..."
+      sudo -u $USER ssh-keygen -t rsa -N "" -f $PRIV_KEY_PATH
+    fi
   fi
 else
   PUB_KEY_PATH=$2
   print_green "Will use this path to SSH public key: $PUB_KEY_PATH"
 fi
 
-PUB_KEY=$(cat $PUB_KEY_PATH)
+PUB_KEY=$(cat ${PUB_KEY_PATH})
 PRIV_KEY_PATH=$(echo ${PUB_KEY_PATH} | sed 's#.pub##')
 CDIR=$(cd `dirname $0` && pwd)
-TECTONIC_LICENSE=$(cat $CDIR/tectonic.lic 2>/dev/null)
-DOCKER_CFG=$(cat $CDIR/docker.cfg 2>/dev/null)
-LIBVIRT_PATH=/var/lib/libvirt/images/coreos
-MASTER_USER_DATA_TEMPLATE=$CDIR/k8s_tectonic_master.yaml
-#MASTER_USER_DATA_TEMPLATE=$CDIR/k8s_master.yaml
-NODE_USER_DATA_TEMPLATE=$CDIR/k8s_node.yaml
+IMG_PATH=${HOME}/libvirt_images/${OS_NAME}
+RANDOM_PASS=$(openssl rand -base64 12)
+MASTER_USER_DATA_TEMPLATE=${CDIR}/k8s_master.yaml
+NODE_USER_DATA_TEMPLATE=${CDIR}/k8s_node.yaml
+ETCD_DISCOVERY=$(curl -s "https://discovery.etcd.io/new?size=$1")
 CHANNEL=alpha
 RELEASE=current
-ETCD_DISCOVERY=$(curl -s "https://discovery.etcd.io/new?size=$1")
-K8S_RELEASE=v1.1.3
+K8S_RELEASE=v1.1.7
 FLANNEL_TYPE=vxlan
-
-ETCD_ENDPOINTS=""
-for SEQ in $(seq 1 $1); do
-  if [ "$SEQ" == "1" ]; then
-		ETCD_ENDPOINTS="http://k8s-master:2379"
-  else
-    NODE_SEQ=$[SEQ-1]
-    ETCD_ENDPOINTS="$ETCD_ENDPOINTS,http://k8s-node-$NODE_SEQ:2379"
-  fi
-done
-
-POD_NETWORK=10.2.0.0/16
-SERVICE_IP_RANGE=10.3.0.0/24
-K8S_SERVICE_IP=10.3.0.1
-DNS_SERVICE_IP=10.3.0.10
-K8S_DOMAIN=cluster.local
+K8S_NET=10.100.0.0/16
+K8S_DNS=10.100.0.254
+K8S_DOMAIN=skydns.local
 RAM=512
 CPUs=1
 IMG_NAME="coreos_${CHANNEL}_${RELEASE}_qemu_image.img"
+IMG_URL="http://${CHANNEL}.release.core-os.net/amd64-usr/${RELEASE}/coreos_production_qemu_image.img.bz2"
 
-if [ ! -d $LIBVIRT_PATH ]; then
-  mkdir -p $LIBVIRT_PATH || (echo "Can not create $LIBVIRT_PATH directory" && exit 1)
+IMG_EXTENSION=""
+if [[ "${IMG_URL}" =~ \.([a-z0-9]+)$ ]]; then
+  IMG_EXTENSION=${BASH_REMATCH[1]}
+fi
+
+case "${IMG_EXTENSION}" in
+  bz2)
+    DECOMPRESS="| bzcat";;
+  xz)
+    DECOMPRESS="| xzcat";;
+  *)
+    DECOMPRESS="";;
+esac
+
+if [ ! -d $IMG_PATH ]; then
+  mkdir -p $IMG_PATH || (echo "Can not create $IMG_PATH directory" && exit 1)
 fi
 
 if [ ! -f $MASTER_USER_DATA_TEMPLATE ]; then
-  echo "Cannot find $MASTER_USER_DATA_TEMPLATE template"
+  echo "$MASTER_USER_DATA_TEMPLATE template doesn't exist"
   exit 1
 fi
 
-if [ ! -f $MASTER_USER_DATA_TEMPLATE ]; then
-  echo "Cannot find $MASTER_USER_DATA_TEMPLATE template"
+if [ ! -f $NODE_USER_DATA_TEMPLATE ]; then
+  echo "$NODE_USER_DATA_TEMPLATE template doesn't exist"
   exit 1
 fi
 
 for SEQ in $(seq 1 $1); do
   if [ "$SEQ" == "1" ]; then
-    COREOS_HOSTNAME="k8s-master"
-    COREOS_MASTER_HOSTNAME=$COREOS_HOSTNAME
+    VM_HOSTNAME="k8s-master"
+    COREOS_MASTER_HOSTNAME=$VM_HOSTNAME
     USER_DATA_TEMPLATE=$MASTER_USER_DATA_TEMPLATE
   else
     NODE_SEQ=$[SEQ-1]
-    COREOS_HOSTNAME="k8s-node-$NODE_SEQ"
+    VM_HOSTNAME="k8s-node-$NODE_SEQ"
     USER_DATA_TEMPLATE=$NODE_USER_DATA_TEMPLATE
   fi
 
-  if [ ! -d $LIBVIRT_PATH/$COREOS_HOSTNAME/openstack/latest ]; then
-    mkdir -p $LIBVIRT_PATH/$COREOS_HOSTNAME/openstack/latest || (echo "Can not create $LIBVIRT_PATH/$COREOS_HOSTNAME/openstack/latest directory" && exit 1)
+  if [ ! -d $IMG_PATH/$VM_HOSTNAME/openstack/latest ]; then
+    mkdir -p $IMG_PATH/$VM_HOSTNAME/openstack/latest || (echo "Can not create $IMG_PATH/$VM_HOSTNAME/openstack/latest directory" && exit 1)
+    sed "s#%PUB_KEY%#$PUB_KEY#g;\
+         s#%HOSTNAME%#$VM_HOSTNAME#g;\
+         s#%DISCOVERY%#$ETCD_DISCOVERY#g;\
+         s#%RANDOM_PASS%#$RANDOM_PASS#g;\
+         s#%MASTER_HOST%#$COREOS_MASTER_HOSTNAME#g;\
+         s#%K8S_RELEASE%#$K8S_RELEASE#g;\
+         s#%FLANNEL_TYPE%#$FLANNEL_TYPE#g;\
+         s#%K8S_NET%#$K8S_NET#g;\
+         s#%K8S_DNS%#$K8S_DNS#g;\
+         s#%K8S_DOMAIN%#$K8S_DOMAIN#g" $USER_DATA_TEMPLATE > $IMG_PATH/$VM_HOSTNAME/openstack/latest/user_data
+    if [ -n "$(selinuxenabled 2>/dev/null || echo 'SELinux')" ]; then
+      # We use ISO configdrive to avoid complicated SELinux conditions
+      mkisofs -input-charset utf-8 -R -V config-2 -o $IMG_PATH/$VM_HOSTNAME/configdrive.iso $IMG_PATH/$VM_HOSTNAME
+      CONFIG_DRIVE="--disk path=$IMG_PATH/$VM_HOSTNAME/configdrive.iso,device=cdrom"
+    else
+      CONFIG_DRIVE="--filesystem $IMG_PATH/$VM_HOSTNAME/,config-2,type=mount,mode=squash"
+    fi
   fi
 
-  if [ ! -f $LIBVIRT_PATH/$IMG_NAME ]; then
-    wget http://${CHANNEL}.release.core-os.net/amd64-usr/${RELEASE}/coreos_production_qemu_image.img.bz2 -O - | bzcat > $LIBVIRT_PATH/$IMG_NAME || (rm -f $LIBVIRT_PATH/$IMG_NAME && echo "Failed to download image" && exit 1)
+  virsh pool-info $OS_NAME > /dev/null 2>&1 || virsh pool-create-as $OS_NAME dir --target $IMG_PATH || (echo "Can not create $OS_NAME pool at $IMG_PATH target" && exit 1)
+  (virsh pool-dumpxml $OS_NAME | virsh pool-define /dev/stdin)
+  virsh pool-start $OS_NAME > /dev/null 2>&1 || true
+
+  if [ ! -f $IMG_PATH/$IMG_NAME ]; then
+    eval "wget $IMG_URL -O - $DECOMPRESS > $IMG_PATH/$IMG_NAME" || (rm -f $IMG_PATH/$IMG_NAME && echo "Failed to download image" && exit 1)
   fi
 
-  if [ ! -f $LIBVIRT_PATH/$COREOS_HOSTNAME.qcow2 ]; then
-    qemu-img create -f qcow2 -b $LIBVIRT_PATH/$IMG_NAME $LIBVIRT_PATH/$COREOS_HOSTNAME.qcow2
-  fi
-
-  sed "s#%PUB_KEY%#$PUB_KEY#g;\
-       s#%HOSTNAME%#$COREOS_HOSTNAME#g;\
-       s#%DISCOVERY%#$ETCD_DISCOVERY#g;\
-       s#%SERVICE_IP_RANGE%#$SERVICE_IP_RANGE#g;\
-       s#%MASTER_HOST%#$COREOS_MASTER_HOSTNAME#g;\
-       s#%K8S_RELEASE%#$K8S_RELEASE#g;\
-       s#%FLANNEL_TYPE%#$FLANNEL_TYPE#g;\
-       s#%POD_NETWORK%#$POD_NETWORK#g;\
-       s#%K8S_SERVICE_IP%#$K8S_SERVICE_IP#g;\
-       s#%DNS_SERVICE_IP%#$DNS_SERVICE_IP#g;\
-       s#%K8S_DOMAIN%#$K8S_DOMAIN#g;\
-       s#%TECTONIC_LICENSE%#$TECTONIC_LICENSE#g;\
-       s#%DOCKER_CFG%#$DOCKER_CFG#g;\
-       s#%ETCD_ENDPOINTS%#$ETCD_ENDPOINTS#g" $USER_DATA_TEMPLATE > $LIBVIRT_PATH/$COREOS_HOSTNAME/openstack/latest/user_data
-  if [[ selinuxenabled ]]; then
-    echo "Making SELinux configuration"
-    semanage fcontext -d -t virt_content_t "$LIBVIRT_PATH/$COREOS_HOSTNAME(/.*)?" || true
-    semanage fcontext -a -t virt_content_t "$LIBVIRT_PATH/$COREOS_HOSTNAME(/.*)?"
-    restorecon -R "$LIBVIRT_PATH"
+  if [ ! -f $IMG_PATH/${VM_HOSTNAME}.qcow2 ]; then
+    virsh pool-refresh $OS_NAME
+    virsh vol-create-as --pool $OS_NAME --name ${VM_HOSTNAME}.qcow2 --capacity 10G --format qcow2 --backing-vol $IMG_NAME --backing-vol-format qcow2 || \
+      qemu-img create -f qcow2 -b $IMG_PATH/$IMG_NAME $IMG_PATH/${VM_HOSTNAME}.qcow2 || \
+      (echo "Failed to create ${VM_HOSTNAME}.qcow2 volume image" && exit 1)
+    virsh pool-refresh $OS_NAME
   fi
 
   virt-install \
     --connect qemu:///system \
     --import \
-    --name $COREOS_HOSTNAME \
+    --name $VM_HOSTNAME \
     --ram $RAM \
     --vcpus $CPUs \
     --os-type=linux \
     --os-variant=virtio26 \
-    --disk path=$LIBVIRT_PATH/$COREOS_HOSTNAME.qcow2,format=qcow2,bus=virtio \
-    --filesystem $LIBVIRT_PATH/$COREOS_HOSTNAME/,config-2,type=mount,mode=squash \
+    --disk path=$IMG_PATH/$VM_HOSTNAME.qcow2,format=qcow2,bus=virtio \
+    $CONFIG_DRIVE \
     --vnc \
-    --noautoconsole
+    --noautoconsole \
+#    --cpu=host
 done
 
-print_green "Use this command to connect to your CoreOS cluster: 'ssh -i $PUB_KEY_PATH core@$COREOS_MASTER_HOSTNAME'"
+print_green "Use this command to connect to your cluster: 'ssh -i $PRIV_KEY_PATH core@$COREOS_MASTER_HOSTNAME'"
